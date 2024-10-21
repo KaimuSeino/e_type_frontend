a3b600380112f3a9a41a3fd98ab87670533f517f
KaimuSeino <skim07a19yki@icloud.com>
2024-10-21 20:24:02 +0900

    英文法レッスンカードの追加

---
+++ app/(main)/(display)/study/grammar/[grammarLessonId]/_components/grammar-play.tsx
@@ -0,0 +1,11 @@
+const GrammarPlay = () => {
+    return (
+        <div className="flex flex-col items-center">
+            <p className="text-3xl font-bold">
+                I (   ) very hungry now. I want something to eat.
+            </p>
+        </div>
+    );
+}
+ 
+export default GrammarPlay;
