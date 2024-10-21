39d3668d9b877a74fdd304aa18f1de2d73d290b7
KaimuSeino <skim07a19yki@icloud.com>
2024-10-21 20:30:56 +0900

    英文法レッスンstudyカードの追加

---
+++ app/(main)/(display)/study/grammar/[grammarLessonId]/_components/grammar-study.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { useState } from "react"
+import GrammarProgress from "./grammar-progress"
+import GrammarReadyCard from "./grammar-ready-card"
+import GrammarPlay from "./grammar-play"
+
+interface GrammarStudyProps {
+    title: string
+    description: string
+    lessonId: number
+}
+
+const GrammarStudy = ({
+    title,
+    description,
+    lessonId
+}: GrammarStudyProps) => {
+    const [isActive, setIsActive] = useState(0)
+
+    const handleStart = () => {
+        setIsActive(1)
+    }
+    return (
+        <>
+            <GrammarProgress
+                numberOfQuestions={5}
+            />
+            {isActive === 0 && (
+                <GrammarReadyCard
+                    title={title}
+                    description={description}
+                    onStart={handleStart}
+                />
+            )}
+            {isActive === 1 && (
+                <GrammarPlay />
+            )}
+        </>
+    );
+}
+ 
+export default GrammarStudy;
