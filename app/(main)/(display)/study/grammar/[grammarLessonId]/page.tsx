48a804c04b56cfdd848f19864862064e982ab4eb
KaimuSeino <skim07a19yki@icloud.com>
2024-11-16 20:42:42 +0900

    シードデータを英文法の型に変更して、英文法学習コンポーネントに渡す

--- app/(main)/(display)/study/grammar/[grammarLessonId]/page.tsx
+++ app/(main)/(display)/study/grammar/[grammarLessonId]/page.tsx
@@ -5,12 +5,39 @@ const GrammarLessonIdPage = ({
 }: {
     params: { grammarLessonId: number}
 }) => {
+    const seedLessonData: GrammarLesson = {
+        id: params.grammarLessonId,
+        title: "動詞の基礎を学ぼう",
+        description: "be動詞と一般動詞の区別",
+    }
+    const seedQuestionData: GrammarQuestion[] = [
+        {
+            id: 1,
+            grammar_lesson_id: params.grammarLessonId,
+            question: "I [] very hungry now. I want something to eat.",
+            choices: "am is are was were",
+            correct_answer: "am",
+            translation: "私は今、とてもお腹が空いています。何か食べたいです。",
+            tip_title: "主語と時制を確認しよう！",
+            tip_description: "空欄の前には主語のIがあるため、選択肢として当てはまるのはamかwasになる。そして一つ目の文の最後にのnowで「今」の話だとわかる",
+        },
+        {
+            id: 2,
+            grammar_lesson_id: params.grammarLessonId,
+            question: "They [] playing soccer yesterday.",
+            choices: "is are was were",
+            correct_answer: "were",
+            translation: "彼らは昨日サッカーをしていました。",
+            tip_title: "They（三人以上）と過去を確認しよう!",
+            tip_description: "主語がTheyで過去形なので、正解はwereです。",
+        }
+    ]
     return (
         <div className="relative flex flex-col items-center gap-y-20">
             <GrammarStudy
-                title="動詞の基礎を学ぼう"
-                description="be動詞と一般動詞の区別"
-                lessonId={params.grammarLessonId}
+                title={seedLessonData.title}
+                description={seedLessonData.description}
+                questions={seedQuestionData}
             />
         </div>
     )
